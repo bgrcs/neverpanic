366c97139879fc844903672d28320e4d54d760ad
liamd-psyomics <liam.debell@psyomics.com>
2026-02-22 21:06:09 +0000

    chore: Add JSDOC

--- index.ts
+++ index.ts
@@ -23,6 +23,31 @@ export const createNeverpanic = <
     error,
   });
 
+  /**
+   * Create a safe function from an unsafe one.
+   *
+   * @param cb - The async function to wrap.
+   * @param [eh] - Optional fallback error handler.
+   * @returns A new function that returns a typesafe Result.
+   *
+   * @example
+   * const getUser = n.safeFn(
+   *   async (id: string) => {
+   *     const res = await fetch(`https://example.com/users/${id}`);
+   *     if (!res.ok) return { success: false, error: "FAILED_TO_FETCH" };
+   *
+   *     return { success: true, data: await res.json() };
+   *   },
+   *   () => "FAILED_TO_GET_USER"
+   * );
+   *
+   * const getUserResult = await getUser("some-user-id");
+   * if (!getUserResult.success) {
+   *   console.error(getUserResult.error);
+   * } else {
+   *   console.log(getUserResult.data);
+   * }
+   */
   const safeFn =
     <
       T extends Result<D, E> | Promise<Result<D, E>>,
@@ -45,6 +70,21 @@ export const createNeverpanic = <
       }
     };
 
+  /**
+   * Run an unsafe function, handle any errors and return a Result.
+   *
+   * @param cb - The async function to call.
+   * @param [eh] - Optional fallback error handler.
+   * @returns The awaited return value of cb.
+   *
+   * @example
+   * const user = await n.fromUnsafe(() => db.findUser('some-user-id'), () => 'FAILED_T0_FIND_USER')
+   * if (!user.success) {
+   * 	console.error(user.error)
+   * } else {
+   * 	console.log(user.data)
+   * }
+   */
   const fromUnsafe = <
     T extends D | Promise<D>,
     EH extends Result<D, E>,
@@ -67,6 +107,22 @@ export const createNeverpanic = <
     }
   };
 
+  /**
+   * Convert a list of results into a single result.
+   *
+   * @param results - A list of Results.
+   * @returns A single result containing the data / errors of the input results.
+   *
+   * @example
+   * const findUserResults = userIds.map((userId) =>
+   *   n.fromUnsafe(
+   *     () => db.findUser(userId),
+   *     () => "FAILED_TO_FIND_USER" as const,
+   *   ),
+   * );
+   *
+   * const result = n.resultsToResult(findUserResults)
+   */
   const resultsToResult = <
     T extends Result<D, E>[],
     TD extends Extract<T[number], { success: true }>,
